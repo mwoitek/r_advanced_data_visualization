b62e2f406b3c4f488b8b2921551265ed30e99b65
Marcio Woitek <woitek@usp.br>
2021-01-09 23:07:44 -0300

    Added the template, and the files I'll actually edit.

---
+++ assignment_2/peer_review_2.R
@@ -0,0 +1,19 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,R:percent,Rmd
+#     text_representation:
+#       extension: .R
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.8.1
+#   kernelspec:
+#     display_name: R
+#     language: R
+#     name: ir
+# ---
+
+# %% [markdown]
+#
+
+# %%
